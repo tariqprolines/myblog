fc66ae2274ddb09e159712de8b56f5c74c554aae
tariqprolines <tariq.prolines.sa@gmail.com>
2019-07-01 16:47:49 +0300

    Commit change password and dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,17 @@ urlpatterns = [
     re_path(r'^register/$',views.register_view,name='register'),
     re_path(r'^login/$',views.login_view,name='login'),
     re_path(r'^logout/$',views.logout,name='logout_account'),
+    re_path(r'^password/$', views.change_password, name='change_password'),
+
     re_path('^', include('django.contrib.auth.urls')),
 
     # Dashboard
 
     re_path(r'^dashboard/$',views.dashboard, name='dashboard'),
-    path('dashboard/<int:p_id>',views.editproject, name='editproject'),
+    path('dashboard/editproject/<int:p_id>',views.editproject, name='editproject'),
+    path('dashboard/deleteproject/<int:p_id>',views.deleteproject, name='deleteproject'),
+
+    # Profile
+
+    re_path(r'dashboard/editprofile/$', views.editprofile, name='editprofile')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
-from django.contrib.auth import login, authenticate,logout as auth_logout
+from django.contrib.auth import login, authenticate,logout as auth_logout, update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 from .forms import RegisterForm
 from project.models import Project
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -33,14 +36,44 @@ def login_view(request):
             messages.success(request, 'Username or Password is not correct!')
     return render(request,'accounts/login.html')
 
+@login_required(login_url='/accounts/login/')
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {
+        'form': form
+    })
+
 def logout(request):
     auth_logout(request)
     return render(request, 'accounts/logout.html')
 
 @login_required(login_url='/accounts/login/')
 def dashboard(request):
+    project_list=Project.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(project_list, 9)
+    try:
+        projectall = paginator.page(page)
+    except PageNotAnInteger:
+        projectall = paginator.page(1)
+    except EmptyPage:
+        projectall = paginator.page(paginator.num_pages)
+
     projects=Project.objects.filter(owner_id=request.user.id)
-    return render(request,'accounts/dashboard.html',{'projects':projects})
+    user=get_object_or_404(User,pk=request.user.id)
+
+    return render(request,'accounts/dashboard.html',{'projectall':projectall,'projects':projects,'user':user})
 
 @login_required(login_url='/accounts/login/')
 def editproject(request,p_id):
@@ -57,3 +90,13 @@ def editproject(request,p_id):
         else:
             messages.success(request, 'Something Wrong!')
     return render(request, 'accounts/editproject.html',{'project':project})
+
+@login_required(login_url='/accounts/login/')
+def deleteproject(request,p_id):
+    delproject=Project.objects.filter(id=p_id).delete()
+    if delproject:
+        messages.success(request, 'Your project has been deleted successfully!')
+        return redirect('dashboard')
+
+def editprofile(request):
+    return HttpResponse("Profile Edit Page")
